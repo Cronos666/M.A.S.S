230932244bb40b9fedd7cba23f969ab8fbcc16d8
Cronos <cronos666@mailbox.org>
2025-12-08 17:03:50 +0100

    Refactor Ball class and add forces calculation
    Refactor Ball class to use velocity instead of momentum and add getter/setter methods. Implement forces calculation function for a list of balls.

--- balls.py
+++ balls.py
@@ -1,12 +1,49 @@
 import numpy as np
 
+#selfexplanatory
 class Ball(object):
-    def __init__(self, mass, radius, position, momentum):
+    def __init__(self, mass, radius, position, velocity):
         self.m=mass
         self.r=radius
         self.pos=np.array(position)
-        self.mo=np.array(momentum)
-        self.vel=self.mo/self.m    
+        self.vel=np.array(velocity)
 
-        
-c=Ball(2,2,(1,2,3),(4,5,6))
+    #getters
+    def getVel(self):
+        return self.vel
+    
+    def getPos(self):
+        return self.pos
+    
+    def getMass(self):
+        return self.m
+    
+    #setters
+    def setVel(self,velo):
+        self.vel=np.array(velo)
+    
+    def setPos(self,pos):
+        self.pos=pos
+
+#forces calculation 
+def forces(list,dt):
+    G=1
+    flist = []
+    for i in list:
+        force = np.zeros(3)
+        for ii in list:
+            if i != ii:
+                vecR = list(i).getPos() - list(ii).getPos()
+                force += (G*list[i].getMass()*list[ii].getMass()/((np.linalg.norm(vecR))**3))*vecR
+        flist.append(force) 
+
+    return flist       
+
+
+
+
+#balls =[]   
+#for i in range (10):
+#    balls.append(Ball(1,1,(i,i+1,i+2),(1,0,1)))
+#balls[0].setVel(np.array((1,2,4)))
+#print(balls[0].getVel())
